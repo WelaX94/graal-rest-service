215fdad0832fc78fc9b554d318e523d6e986c0e6
welax94 <welax94@gmail.com>
2021-08-09 17:02:56 +0300

    Added JS Engine availability check

--- src/main/java/com/project/graalrestservice/GraalRestServiceApplication.java
+++ src/main/java/com/project/graalrestservice/GraalRestServiceApplication.java
@@ -1,13 +1,27 @@
 package com.project.graalrestservice;
 
+import org.graalvm.polyglot.Context;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class GraalRestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraalRestServiceApplication.class, args);
+		boolean jsDetected = false;
+		Context context = Context.newBuilder().build();
+		for(String s: context.getEngine().getLanguages().keySet()) {
+			if (s.equals("js")) jsDetected = true;
+		}
+		context.close();
+		if (jsDetected) {
+			SpringApplication.run(GraalRestServiceApplication.class, args);
+		}
+		else {
+			System.out.println("JS Engine not found. The program is closing");
+		}
 	}
 
 }
