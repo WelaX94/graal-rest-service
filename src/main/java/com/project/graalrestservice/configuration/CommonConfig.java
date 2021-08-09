ac5cfbe7491dd06549a87dc269bb7c6d70fd3dbc
welax94 <welax94@gmail.com>
2021-08-09 09:12:45 +0300

    Removed the explicit announcement of beans

--- src/main/java/com/project/graalrestservice/configuration/CommonConfig.java
+++ src/main/java/com/project/graalrestservice/configuration/CommonConfig.java
@@ -1,9 +1,5 @@
 package com.project.graalrestservice.configuration;
 
-import com.project.graalrestservice.domain.services.ScriptService;
-import com.project.graalrestservice.domain.services.ScriptRepository;
-import com.project.graalrestservice.domain.services.serviceImplementations.ScriptServiceImpl;
-import com.project.graalrestservice.domain.services.serviceImplementations.ScriptRepositoryImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,18 +14,8 @@ import java.util.concurrent.Executors;
 public class CommonConfig {
 
     @Bean
-    public ScriptService scriptHandler() {
-        return new ScriptServiceImpl();
-    }
-
-    @Bean
     public ExecutorService executorService() {
         return Executors.newFixedThreadPool(10);
-    }
-
-    @Bean
-    public ScriptRepository scriptList() {
-        return new ScriptRepositoryImpl();
     }
 
 }
